93e7836ec3f70efc38e4c88959bb0470dd6adf5a
Аня Киселёва <avelesik007@gmail.com>
2018-09-28 17:03:25 +0300

    111

--- anketaApp/Controllers/HomeController.cs
+++ anketaApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace anketaApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descriptionpage.";
 
             return View();
         }
